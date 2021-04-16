dd65babfb3e34c1c8fae899698a5339d3c3b6a0b
shakhzad1211 <48676441+shakhzad1211@users.noreply.github.com>
2021-04-16 09:44:00 +0500

    Add files via upload

---
+++ _database/database.php
@@ -0,0 +1,8 @@
+<?php
+    $hostname = "localhost:3306";
+    $user = "root";
+    $password = "root";
+    $database = "articles";
+    $prefix = "";
+    $database=mysqli_connect($hostname,$user,$password,$database);
+?>
